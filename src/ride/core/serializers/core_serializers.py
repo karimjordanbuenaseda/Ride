308198f8b97eb22e0d164ed26ef7c326104a5f6a
karimjordanbuenaseda <karimjordanbuenaseda@gmail.com>
2025-05-07 04:52:49 +0800

    Refactor CoreSerializer to inherit from ModelSerializer for improved functionality

--- src/ride/core/serializers/core_serializers.py
+++ src/ride/core/serializers/core_serializers.py
@@ -1,25 +1,23 @@
 from rest_framework import serializers
 
-class CoreSerializer(serializers.Serializer):
+class CoreSerializer(serializers.ModelSerializer):
     
-    def _init__(self, *args, **kwargs):
-        def __init__(self, *args, **kwargs):
-            """
-            Initialize the CoreSerializer with optional field filtering.
-            This method customizes the serializer to only include specific fields
-            if the 'fields' parameter is provided in kwargs.
-            Args:
-                *args: Variable length argument list passed to parent initializer
-                **kwargs: Arbitrary keyword arguments
-                          - fields (list, optional): List of field names to include in the serializer.
-                            If provided, only these fields will be kept in the serializer.
-            Note:
-                The 'fields' parameter is popped from kwargs before passing to the parent initializer.
-                If 'fields' is provided, any field not in the list will be removed from the serializer.
-            """
-        
-        super(CoreSerializer, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize the CoreSerializer with optional field filtering.
+        This method customizes the serializer to only include specific fields
+        if the 'fields' parameter is provided in kwargs.
+        Args:
+            *args: Variable length argument list passed to parent initializer
+            **kwargs: Arbitrary keyword arguments
+                        - fields (list, optional): List of field names to include in the serializer.
+                        If provided, only these fields will be kept in the serializer.
+        Note:
+            The 'fields' parameter is popped from kwargs before passing to the parent initializer.
+            If 'fields' is provided, any field not in the list will be removed from the serializer.
+        """
         fields = kwargs.pop('fields', None)
+        super(CoreSerializer, self).__init__(*args, **kwargs)
 
         if fields is not None:
             allowed = set(fields)
